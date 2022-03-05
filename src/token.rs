0dee260fe1f40a1f17e36d6be1cf655fd9de0076
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-05 13:02:20 +0530

    feat(): implement Token enum

---
+++ src/token.rs
@@ -0,0 +1,56 @@
+use std::fmt::{self, Display, Formatter};
+
+
+pub enum Token {
+    Eof,
+    Illegal,
+
+    // Literals
+    Ident(String),
+    Int(i32),
+    String(String),
+    Boolean(bool),
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Percent,
+
+    // Comparison
+    Less,
+    Greater,
+    LessEqual,
+    GreaterEqual,
+    Equals,
+    NotEquals,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+
+    // Keywords
+    Let,
+    Func,
+    If,
+    Else,
+    Return,
+    Import
+
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
