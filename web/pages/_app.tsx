b20752f03ffea517628cbf57675c6f1e3e391c6b
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-06 20:59:25 +0530

    feat(): add web

---
+++ web/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />
+}
+export default MyApp
