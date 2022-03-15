b66483a78152169ad9853b0f6de93a2906228e11
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-15 12:49:54 +0530

    feat(std): add `to_string()` to string std library

--- src/std_library/string.rs
+++ src/std_library/string.rs
@@ -8,6 +8,7 @@ use super::Res;
 pub fn add_globals() -> Res {
     let mut globals = HashMap::new();
     globals.insert(String::from("replace"), Object::Inbuilt(replace));
+    globals.insert(String::from("to_string"), Object::Inbuilt(to_string));
     Res {
         globals,
         raw: None,
@@ -29,5 +30,32 @@ pub fn replace(args: Vec<Object>) -> Object {
             Object::String(s)
         }
         o => Object::Error(format!("First argument must be a string. Got {}", o)),
+    }
+}
+
+pub fn to_string(args: Vec<Object>) -> Object {
+    if args.len() != 1 {
+        return Object::Error(format!(
+            "Wrong number of arguments. Got {}. Expected 1.",
+            args.len()
+        ));
+    }
+
+    match &args[0] {
+        Object::String(s) => Object::String(s.clone()),
+        Object::Array(a) => {
+            let mut s = String::new();
+            for o in a.iter() {
+                s.push_str(&o.to_string());
+            }
+            Object::String(s)
+        },
+        Object::Number(n) => Object::String(n.to_string()),
+        Object::Bool(b) => Object::String(b.to_string()),
+        Object::Null => Object::String(String::from("null")),
+        Object::Error(e) => Object::String(e.clone()),
+        Object::Fn(..) => Object::String(String::from("[Function]")),
+        Object::Inbuilt(..) => Object::String(String::from("[Inbuilt Function]")),
+        o => Object::String(format!("{}", o))        
     }
 }
